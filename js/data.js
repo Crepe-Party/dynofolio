7bd6647af9c8409f1b6b0a524b71c02f22b17a63
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-09-28 09:33:21 +0200

    fill data file

--- js/data.js
+++ js/data.js
@@ -1,5 +1,46 @@
 var mainData = {
     me:{
-        firstName: "Robert"
-    }
+        firstName: "Robert",
+        lastname: "Wood",
+        adress: "Rue du bois 24, 1450 Ste-Croix",
+        phone: "079 970 07 70",
+        mail: "robert.fistiniere@mail.ch",
+        birthDate: "01.01.1990",
+        status: "Célibataire",
+        nationality: "Suisse",
+        goals: "Je souhaite couper un baobab",
+        others:[
+            "j'aime couper du bois",
+            "J'ai le permis camion",
+        ]
+    },
+    jobs:[
+        {
+            name: "training1",
+            location: "forêt",
+            startDate: "01-02-2001",
+            endDate: "",
+            description: "Cuting tree",
+            tasks:[
+                "Task 1",
+                "Task 2",
+                "Task 3"
+            ],
+            skillsDeveloped:[
+                "Skill 1",
+                "Skill 2",
+                "Skill 3"
+            ]
+        }
+    ],
+    skills:[
+        {
+            name: "skill 1",
+            masteryLevelOf10: 6,
+            proofOfCompetence:[
+                "proof 1",
+                "proof 2"
+            ]
+        }
+    ]
 }
