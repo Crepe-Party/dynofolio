3fe4f133d0ed7ef2c498580a52dfa36d4a5a257f
Florian Bergmann <flobergmann@aol.ch>
2020-10-20 18:13:19 +0200

    add switch category

--- js/dynofolio.js
+++ js/dynofolio.js
@@ -3,4 +3,14 @@ function boot(){
     console.log(mainData);
     buildWithData();
     document.body.classList.remove("loading");
+        
+    document.querySelectorAll(".menu-child").forEach(child => child.addEventListener("click", menu => {
+        document.querySelectorAll(".menu-child").forEach(child => child.classList.remove("menu-active"));
+        document.querySelectorAll(".content-child").forEach(child => child.classList.add("hidden"));
+        
+        var menu_id = menu.target.id;
+        var content_id = menu_id.split("-").pop()
+        document.getElementById(menu_id).classList.add("menu-active");
+        document.getElementById(content_id).classList.remove("hidden");
+    }));
 }
