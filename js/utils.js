65ea44b99c848ffa9f3b1bfe488fca1a5da84110
Nicolas Maitre <nicolas.johan.maitre@gmail.com>
2020-11-11 08:43:44 +0100

    page builder simple documentation

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,4 @@
 Object.valFromPath = function(object, array){
-    console.log("obj", object, array)
-    // val = Object.assign({}, object);
     val = object
     array.forEach(path=>{
         val = val[path]
